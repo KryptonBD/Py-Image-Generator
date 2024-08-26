ce2871b66ebf92a17f59a57e9ea76309e70d9ff3
KryptonBD <foysol.sahi@gmail.com>
2024-08-26 22:04:52 +0100

    Connect with OpenAI

--- main.py
+++ main.py
@@ -1,13 +1,42 @@
 from kivy.app import App
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.lang import Builder
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+import requests
+from datetime import datetime
+
+
+load_dotenv()
+
+apikey = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=apikey)
 
 Builder.load_file("index.kv")
 
 
 class IndexScreen(Screen):
     def generate_image(self):
-        self.manager.current_screen.ids.img.source = "home.jpg"
+        # Get prompt enter by the user
+        query = self.manager.current_screen.ids.user_prompt.text
+
+        response = client.images.generate(
+            model="dall-e-3",
+            prompt=query,
+            size="1024x1024",
+            quality="standard",
+            n=1,
+        )
+
+        image_url = response.data[0].url
+        req = requests.get(image_url)
+
+        image_path = f"output/{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.jpg"
+        with open(image_path, "wb") as file:
+            file.write(req.content)
+
+        self.manager.current_screen.ids.img.source = image_path
 
 
 class RootWidget(ScreenManager):
