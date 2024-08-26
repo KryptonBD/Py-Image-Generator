7f27e04f11dd6f56bd17fe233bb523d6364e4a78
KryptonBD <foysol.sahi@gmail.com>
2024-08-26 22:27:29 +0100

    Clean up

--- main.py
+++ main.py
@@ -13,30 +13,57 @@ load_dotenv()
 apikey = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=apikey)
 
+# Load Kivy file
 Builder.load_file("index.kv")
 
 
 class IndexScreen(Screen):
-    def generate_image(self):
-        # Get prompt enter by the user
-        query = self.manager.current_screen.ids.user_prompt.text
 
+    def generate_image(self, prompt):
+        """
+        Generate an image with OPEN AI's DALL-E-3 model
+        """
         response = client.images.generate(
             model="dall-e-3",
-            prompt=query,
+            prompt=prompt,
             size="1024x1024",
             quality="standard",
             n=1,
         )
 
-        image_url = response.data[0].url
-        req = requests.get(image_url)
+        return response.data[0]
 
-        image_path = f"output/{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.jpg"
-        with open(image_path, "wb") as file:
-            file.write(req.content)
+    def get_image(self, image):
+        """
+        Download the generated image
+        """
+        return requests.get(image.url)
 
-        self.manager.current_screen.ids.img.source = image_path
+    def save_image(self, image):
+        """
+        Save the downloaded image
+        """
+        self.image_path = f"output/{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.jpg"
+
+        with open(self.image_path, "wb") as file:
+            file.write(image.content)
+
+    def set_image(self):
+        """
+        Set the image as the source of the image widget
+        """
+        self.manager.current_screen.ids.img.source = self.image_path
+
+    def submit_prompt(self):
+        """
+        Get the user propmt, generate the image, download the image and display the image
+        """
+        query = self.manager.current_screen.ids.user_prompt.text
+
+        data = self.generate_image(query)
+        image = self.get_image(data)
+        self.save_image(image)
+        self.set_image()
 
 
 class RootWidget(ScreenManager):
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Kivy OpenAI image generator
+
+This is a simple GUI application created with Python's Kivy and OpenAI's Dall-e model to generate image based on the user prompt.
+
+# PrePrerequisites
+
+- An OpenAI API key
